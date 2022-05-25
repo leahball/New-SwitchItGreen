dd837d09987e6db99d6e8bbf4da211185dd5879f
iamleahb <leah.ball@gmail.com>
2022-05-25 14:43:30 -0500

    tried adding Silka fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import Fonts from "../public/theme/Fonts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   );
